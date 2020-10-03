f9070b8a848c219da8d8abc34f75e50d464edd36
inho <balentinoih@gmail.com>
2020-10-03 15:09:43 +0900

    MoveCmd
    움직임관련

--- bomb_of_lee/bomb_of_lee/api/models.py
+++ bomb_of_lee/bomb_of_lee/api/models.py
@@ -2,5 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # 폭탄을 돌리는 주체
-class Inhee(models.Model):
-    pass
+class MoveCmd(models.Model):
+    user = models.CharField
+    direction = models.CharField
